e67767880882a6b7bb56b4f8dd2d4998c50dcd02
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2022-07-06 13:33:20 +0200

    Set up nRF52840 to act as mesh node
    Signed-off-by: Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>

--- samples/gateway_nRF52840/src/main.c
+++ samples/gateway_nRF52840/src/main.c
@@ -6,6 +6,10 @@
 
 #include <zephyr/logging/log.h>
 #include <zephyr/device.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/mesh.h>
+#include <dk_buttons_and_leds.h>
+#include <bluetooth/mesh/dk_prov.h>
 
 #define MODULE main
 
@@ -17,9 +21,53 @@ LOG_MODULE_REGISTER(MODULE);
 
 static const struct device *mesh_uart = DEVICE_DT_GET(DT_NODELABEL(uart1));
 
+static int setup_mesh()
+{
+	int err;
+	err = bt_enable(NULL);
+	if (err)
+	{
+		LOG_ERR("Failed to initialize bluetooth: Error %d", err);
+		return err;
+	}
+	LOG_DBG("Bluetooth initialized");
+	err = bt_mesh_init(bt_mesh_dk_prov_init(), model_handler_init());
+	if (err)
+	{
+		LOG_ERR("Failed to initialize mesh: Error %d", err);
+		return err;
+	}
+
+	if (IS_ENABLED(CONFIG_SETTINGS))
+	{
+		err = settings_load();
+		if (err)
+		{
+			LOG_ERR("Failed to load settings: Error %d", err);
+		}
+	}
+
+	err = bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+	if (err == -EALREADY)
+	{
+		LOG_DBG("Device already provisioned");
+		LOG_DBG("Mesh initialized");
+		return 0;
+	}
+	return 0;
+}
+
 void main(void)
 {
-	int err = init_uart(mesh_uart);
+	int err = 0;
+	err = setup_mesh();
+	if (err)
+	{
+		LOG_ERR("Failed to setup mesh: Error %d", err);
+		return;
+	}
+
+	err = init_uart(mesh_uart);
 	if (err)
 	{
 		LOG_ERR("Could not initialize UART: Error %d", err);
