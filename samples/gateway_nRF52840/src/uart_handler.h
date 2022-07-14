becd7f2bf2d6a71ed3e1ad38bc0de6c4ececcc5e
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2022-07-14 08:36:40 +0200

    Add missing include
    Signed-off-by: Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>

--- samples/gateway_nRF52840/src/uart_handler.h
+++ samples/gateway_nRF52840/src/uart_handler.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <zephyr/device.h>
 #include "../../common/nRF9160dk_uart_interface/messages.h"
+#include "robot_movement_cli.h"
 
 int init_uart(const struct device *dev, struct bt_mesh_robot_config_cli *config_client);
 
